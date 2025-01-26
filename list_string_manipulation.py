f69046a8542f604b7d35dfb1e03b17bd87361d3e
Odlanyehr Boral <OdlanyehrBoral@gmail.com>
2025-01-26 17:17:32 +0800

    working list and diffrent prints

--- list_string_manipulation.py
+++ list_string_manipulation.py
@@ -1,12 +1,27 @@
 names = []
 
-input = input("Do you want to add a name to the list? ")
-if input == 'yes' or input == 'Yes' or input == 'Y' or input == 'y':
-    names.append(input)
-    print(names)
-else:
-    input == 'no' or input == 'No' or input == 'N' or input == 'n'
+input_name = input("Do you want to add a name to the list? ")
+if input_name == "yes" or input_name == "Yes" or input_name == "YES" or input_name == "Y" or input_name == "y":
+    add = input("Enter name here. ")
+    names.append(add)
+    print(f"Added {add} to list")
+elif input_name == "no" or input_name == "No" or input_name == "NO" or input_name == "N" or input_name == "n":
     print("No names added.")
+else:
+    print("invalid input, enter yes or no")
 
+print(names)
+
+uppercase_names = [name.upper() for name in names]
+sorted_names = sorted(names)
+reverse_sorted_names = sorted(names, reverse=True)
     
+    # Remove vowels from names
+vowels = "aeiouAEIOU"
+names_without_vowels = [''.join([char for char in name if char not in vowels]) for name in names]
     
+print("\nOriginal Names:", names)
+print("Uppercase Names:", uppercase_names)
+print("Sorted Names:", sorted_names)
+print("Reverse Sorted Names:", reverse_sorted_names)
+print("Names Without Vowels:", names_without_vowels)
