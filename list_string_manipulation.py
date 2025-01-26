23668e521c5b6b3a75aba096473f9b9a8e406dc8
Odlanyehr Boral <OdlanyehrBoral@gmail.com>
2025-01-26 16:09:56 +0800

    list

--- list_string_manipulation.py
+++ list_string_manipulation.py
@@ -1,2 +1,12 @@
+names = []
+
 input = input("Do you want to add a name to the list? ")
-print(input)
+if input == 'yes' or input == 'Yes' or input == 'Y' or input == 'y':
+    names.append(input)
+    print(names)
+else:
+    input == 'no' or input == 'No' or input == 'N' or input == 'n'
+    print("No names added.")
+
+    
+    
