86694f0f980e23d2c9cbe4d523ac924a4944dbab
Odlanyehr Boral <OdlanyehrBoral@gmail.com>
2025-01-26 18:22:04 +0800

    ask to delete name and delete function

--- list_string_manipulation.py
+++ list_string_manipulation.py
@@ -16,7 +16,6 @@ uppercase_names = [name.upper() for name in names]
 sorted_names = sorted(names)
 reverse_sorted_names = sorted(names, reverse=True)
     
-    # Remove vowels from names
 vowels = "aeiouAEIOU"
 names_without_vowels = [''.join([char for char in name if char not in vowels]) for name in names]
     
@@ -25,3 +24,34 @@ print("Uppercase Names:", uppercase_names)
 print("Sorted Names:", sorted_names)
 print("Reverse Sorted Names:", reverse_sorted_names)
 print("Names Without Vowels:", names_without_vowels)
+
+#make a ask delte name and delete name
+delete = input("Do you want to delete a name from the list? ")
+if delete == 'yes' or delete == 'Yes' or delete == 'Y' or delete == 'y':
+    name_delete = input("Enter the name you want to delete: ")
+    if name_delete in names:
+            names.remove(name_delete)
+            print(f"Deleted: {name_delete}")
+    else:
+            print(f"Name '{name_delete}' not found in the list.")
+            
+elif delete == 'no' or delete == 'No' or delete == 'N' or delete == 'n':
+        print("No names deleted.")
+else:
+        print("Invalid input. Please enter 'yes' or 'no'.")
+        
+uppercase_names = [name.upper() for name in names]
+sorted_names = sorted(names)
+reverse_sorted_names = sorted(names, reverse=True)
+    
+vowels = "aeiouAEIOU"
+names_without_vowels = [''.join([char for char in name if char not in vowels]) for name in names]
+    
+print("\nOriginal Names:", names)
+print("Uppercase Names:", uppercase_names)
+print("Sorted Names:", sorted_names)
+print("Reverse Sorted Names:", reverse_sorted_names)
+print("Names Without Vowels:", names_without_vowels)        
+
+        
+#ask if want to input name agian.                
